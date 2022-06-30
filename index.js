f160f9cca4d110759ef2aef5bf732510545bd134
Yemi George <yg@yg.com>
2022-06-30 16:50:14 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//import * as fs from 'node:fs/promises';
+
+// import { copyFileSync, constants } from 'node:fs';
+
+// copyFileSync('file1.txt', 'file1copy2.txt', constants.COPYFILE_EXCL);
+
+//Super heroes
+var superheroes = require("superheroes");
+var hero1 = superheroes.random();
+console.log(hero1);
+
+
+//Super Villains
+const supervillains = require("supervillains");
+var villain1 = supervillains.random();
+console.log(villain1);
